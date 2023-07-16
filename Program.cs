30f38e3e7b726a9062cde44fdf18cc68588dce46
Quiholder <119394603+Quiholder@users.noreply.github.com>
2023-07-16 11:25:53 -0400

    add files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting");
+            string name = Console.ReadLine();
+            Console.WriteLine("Remember we're accounting on you!");
+            Console.Read();
+        }
+    }
+}
