ca7aa52d010589c18a29f02b2cbc78526d68eb37
QUIANA <quiana242@gmail.com>
2023-07-28 13:04:18 -0700

    booleanlogic

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your age?");
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your age is: " + yourAge);
+            Console.ReadLine();
+
+            Console.WriteLine("have you ever had a dui?");
+            bool anyDui = false;
+            Console.ReadLine();
+            
+
+            Console.WriteLine("how many speeding tickets do you have?");
+            int speedTicket = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            bool canGetIns = (yourAge >= 15 anyDui && speedTicket < 3);
+            Console.WriteLine("Are you qualified? " + canGetIns);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
