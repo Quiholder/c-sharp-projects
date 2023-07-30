3bdeb04ec967b9aedba69a9a0d69e24dddcd4026
QUIANA <quiana242@gmail.com>
2023-07-30 09:53:20 -0700

    completed

---
+++ DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 5;
+            do
+            {
+                switch (number)
+                {
+                    case 25:
+                        Console.WriteLine("you guessed 25! sorry, try again!");
+                        Console.WriteLine("Guess a number!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 12:
+                        Console.WriteLine("you guessed 12! sorry, try again!");
+                        Console.WriteLine("Guess a number!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    // this result is showing up on console twice when ran not sure why
+                    case 5:
+                        Console.WriteLine("you guessed 5! That is correct!");
+                        isGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You haven't guessed correctly yet. Keep trying!");
+                        Console.WriteLine("Guess a number!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+            //! same as isGuessed = false
+            while (!isGuessed);
+            {
+                switch (number)
+                {
+                    case 25:
+                        Console.WriteLine("you guessed 25! sorry, try again!");
+                        Console.WriteLine("Guess a number!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 12:
+                        Console.WriteLine("you guessed 12! sorry, try again!");
+                        Console.WriteLine("Guess a number!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                 // this result is showing up on console 3x when ran not sure why
+                    case 5:
+                        Console.WriteLine("you guessed 5! That is correct!");
+                        isGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You haven't guessed correctly yet. Keep trying!");
+                        Console.WriteLine("Guess a number!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            switch(number)
+            {
+                case 25:
+                    Console.WriteLine("you guessed 25! sorry, try again!");
+                    break;
+
+                case 12:
+                    Console.WriteLine("you guessed 12! sorry, try again!");
+                    break;
+                
+                case 5:
+                    Console.WriteLine("you guessed 5! That is correct!");
+                    break;
+
+                default:
+                    Console.WriteLine("You haven't guessed correctly yet. Keep trying!");
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
