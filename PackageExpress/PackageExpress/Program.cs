e0293d72dd999a3cd93c456c4c7ec411b4635a76
QUIANA <quiana242@gmail.com>
2023-07-31 17:40:41 -0700

    package express updte

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -16,27 +16,33 @@ namespace PackageExpress
             if (packWeight > 50)
             {
                 Console.WriteLine("Your package is too heavy to shipped via Package Express. Have a great day!");
+                Console.ReadLine();
             }
-            Console.ReadLine();
-            
+            else
+            {
+                Console.WriteLine("What is the package width?");
+                int packWidth = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("What is the package width?");
-            int packWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the package height?");
+                int packHeight = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("What is the package height?");
-            int packHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the package length?");
+                int packLength = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("What is the package length?");
-            int packLength = Convert.ToInt32(Console.ReadLine());
-
-            string totalDimension = packWidth + packHeight + packLength > 50 ? "package too heavy to ship." : "your package can be shipped!";
-            Console.WriteLine(totalDimension);
-            Console.ReadLine();
-
-            int quote = packHeight * packWidth * packLength * packWeight / 100;
-            Console.WriteLine("your estimated total to ship this package is $ " + quote +  " Thank you");
-            Console.ReadLine();
-
+                int totalDimension = packWidth + packHeight + packLength;
+                if (totalDimension > 50)
+                {
+                    Console.WriteLine("Package is too bigg to ship.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Your package can be shipped!");
+                    int quote = packHeight * packWidth * packLength * packWeight / 100;
+                    Console.WriteLine("your estimated total to ship this package is $ " + quote + " Thank you");
+                    Console.ReadLine();
+                }
+            }
 
         }
     }
