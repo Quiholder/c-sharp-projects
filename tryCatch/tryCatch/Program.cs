1cfca1b0bf74bd2069e3637b3617ce182b3825fc
QUIANA <quiana242@gmail.com>
2023-08-03 17:39:39 -0700

    trycatch completed

--- tryCatch/tryCatch/Program.cs
+++ tryCatch/tryCatch/Program.cs
@@ -10,13 +10,48 @@ namespace tryCatch
     {
         static void Main(string[] args)
         {
-            int[] Numbers = { 10, 40, 20 };
-            Console.WriteLine("pick a number");
-            int numberOne = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < Numbers.Length; i++)
+            List<int> numbers = new List<int>(){ 10, 40, 20, 30, 60 };
+            try
             {
-                if (i / numberOne; i++);
+                Console.WriteLine("pick a number");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                foreach (var item in numbers)
+                {
+                    Console.WriteLine(item / numberOne);
+                }
+                //for (int i = 0; i < numbers.Count; i++)
+                //{
+                //    Console.WriteLine(numbers[i] / numberOne);
+                //}
             }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("pls pick a number");
+                
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("pls do not divide by 0.");
+            }
+            catch (Exception ex)// general exeption
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+
+        }
     }
 }
