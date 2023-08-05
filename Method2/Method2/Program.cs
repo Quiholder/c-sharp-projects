8c0ed80a0c3c1d5ffcf072d3df757c875cc7908c
QUIANA <quiana242@gmail.com>
2023-08-05 09:36:18 -0700

    method2 complete

---
+++ Method2/Method2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            method2 obj = new method2();
+            Console.WriteLine("Please enter a number...");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number...");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(obj.Addition(number + number2));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method2/Method2/method2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class method2
+    {
+        public int Addition(int num1 = 5, int num2 = 8)
+        {
+            return num1 + num2;
+        }
+    }
+}
