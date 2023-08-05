0d064ae78b9a084386e4772ad4c5b19297fe564e
QUIANA <quiana242@gmail.com>
2023-08-05 09:19:27 -0700

    main method

---
+++ MainMethod/MainMethod/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class MyClass
+    {
+        public int Addition(int num)
+        {
+            return num + 5;
+        }
+        public double Subtraction(double num)
+        {
+            return num - 10.00;
+        }
+        public string Multiply(string num)
+        {
+            return num ;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            Console.WriteLine("Please enter a number...");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(obj.Addition(number));
+            
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(obj.Subtraction(number1));
+            Console.ReadLine();
+
+            Console.WriteLine("Spell out a number");
+            string number2 = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(obj.Multiply(number2));
+            Console.ReadLine();
+
+        }
+    }
+}
