91c7bd2fa4e3e2235d15fe18725cec8e4ac185db
QUIANA <quiana242@gmail.com>
2023-08-12 21:51:36 -0700

    lambda

---
+++ Lamda/Lamda/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamda
+{
+    class Employee
+    {
+        public List<string> Persons { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Id { get; set; }
+    }
+}
---
+++ Lamda/Lamda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> empList = new List<string>() {"Joe", "Mary", "Lewis","Nikki", "Harry", "Sally", "Joe","Jen", "Danielle","Britney"};
+            Employee employee = new Employee();
+            string count = employee.FirstName.Count(x => x.FirstName == employee.FirstName);//lamba expression
+            int sum = IdList.Where(x => x > 5);
+            Console.WriteLine(Where);
+            foreach (Employee employee in employee.FirstName)
+            {
+                if (employee.FirstName == "Joe")
+                {
+                    new List();
+                }
+                Console.WriteLine(new List);
+            }
+
+        }
+    }
+}
