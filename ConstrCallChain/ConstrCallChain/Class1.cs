e8ee74804a05b336cedc3e7e5473343a46bfadfc
QUIANA <quiana242@gmail.com>
2023-08-15 22:51:56 -0700

    call chain

---
+++ ConstrCallChain/ConstrCallChain/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrCallChain
+{
+    class Class1
+    {
+        public Class1(string name) : this (name, 100)
+        {
+        }
+        public Class1(string name, int newBalance)
+        {
+            Quarter = new List<Card>();
+            Money = newBalance;
+            Name = name;
+        }
+        public List<Card> Quarter { get; set; }
+        public int Money { get; set; }
+    }
+}
---
+++ ConstrCallChain/ConstrCallChain/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrCallChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newClass1 = new Class1("qui");
+            const string storeName = "Walmart";
+
+        }
+    }
+}
