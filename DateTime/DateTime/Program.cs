e440f2561d3163b73be0b5d69f9e81e43e83e39f
QUIANA <quiana242@gmail.com>
2023-08-16 18:22:11 -0700

    date time

--- DateTime/DateTime/Program.cs
+++ DateTime/DateTime/Program.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DateTime
+namespace TimeDate
 {
     class Program
     {
         static void Main(string[] args)
 
         {
-            
+            //Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
             DateTime currentDateTime = DateTime.Now;
             Console.WriteLine("Current date and time: " + currentDateTime);
             Console.ReadLine();
+
+            //Asks the user for a number.
             Console.WriteLine("Please enter a number...");
             int userNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("in" + userNumber + "hours it will be" + currentDateTime + userNumber);
