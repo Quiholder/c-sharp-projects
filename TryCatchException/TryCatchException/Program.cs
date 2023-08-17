a41ad431956c3b531b8b9dc7b5db8fbed69ccc3c
QUIANA <quiana242@gmail.com>
2023-08-16 19:55:17 -0700

    exceptions

---
+++ TryCatchException/TryCatchException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("what year were you born?");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred. Do not enter 0 or a negative number.");
+                Console.ReadLine();
+               
+
+            }
+        }
+    }
+}
