049054fe0c14c4dc4d81e2ceec54721779e5a04b
QUIANA <quiana242@gmail.com>
2023-08-27 13:34:32 -0700

    myfirstazure

---
+++ MyFirstAzureWebApp1/MyFirstAzureWebApp1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstAzureWebApp1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToShortDateString();
+            ViewData["TimeStamp"] = dateTime;
+            
+        }
+    }
+}
